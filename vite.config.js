ea616c111af64f72a6525320812de87dc8061855
Błażej Plichta <b.plichta96@gmail.com>
2023-04-27 13:14:17 +0200

    fixed bundling settings

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
+import { resolve } from "path";
+
+const root = resolve(__dirname, "src");
+
 export default {
   base: "/SpaceFeed/",
-  root: "./src/",
+  root,
   build: {
     outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+        library: resolve(root, "library.html"),
+      },
+    },
   },
   server: {
     port: 8080,
